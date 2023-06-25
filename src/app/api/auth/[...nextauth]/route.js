a45d891002df26358c58db86edb6ff33953c3865
Anurag-Bharati <anuragbharati26@gmail.com>
2023-06-25 22:40:58 +0545

    Seperated google login button from auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { firestore } from "@/config/firebase/firebase";
+import { firebaseApp, firestore } from "@/config/firebase/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
@@ -9,7 +11,27 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const auth = getAuth(firebaseApp);
+        try {
+          const res = await signInWithEmailAndPassword(auth, credentials.email, credentials.password);
+          return res.user;
+        } catch (error) {
+          throw new Error("Email or Password is incorrect");
+        }
+        // Any object returned will be saved in `user` property of the JWT
+        // If you return null then an error will be displayed advising the user to check their details.
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      },
+    }),
   ],
+  secret: process.env.SECRET,
   adapter: FirestoreAdapter(firestore),
   pages: { signIn: "/auth" },
 };
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,28 +1,57 @@
 "use client";
 import { userState } from "@/atoms/userAtom";
-import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import { BiChevronLeft, BiLoader } from "react-icons/bi";
+import { useEffect, useRef, useState } from "react";
+import { BiChevronLeft } from "react-icons/bi";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { GrGoogle } from "react-icons/gr";
 import { useRecoilState } from "recoil";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseApp } from "@/config/firebase/firebase";
+import GoogleAuthButton from "./GoogleAuthButton";
+import { signIn } from "next-auth/react";
 
 const Auth = () => {
-  const [state, setState] = useState({ loading: false });
+  const [state, setState] = useState({ loading: false, error: null });
+  const auth = getAuth(firebaseApp);
+
+  const loginFormRef = useRef(null);
+  const signupFormRef = useRef(null);
 
   const [user, setUser] = useRecoilState(userState);
   const [isLogin, setIsLogin] = useState(true);
   const [showPass, setShowPass] = useState(false);
-
   const toggleShowPass = () => setShowPass(!showPass);
 
-  const handleGoogleSignIn = async () => {
-    setState({ loading: true });
-    await signIn("google", { callbackUrl: "http://localhost:3000" });
-    setState({ loading: false });
+  const handleEmailSignIn = (e) => {
+    e.preventDefault();
+    setState({ loading: true, error: null });
+    var { email, password } = document.forms[0];
+    signIn("credentials", { email: email.value, password: password.value, callbackUrl: "http://localhost:300" });
   };
+  // add user session to next-auth
+
+  const handleEmailSignUp = (e) => {
+    e.preventDefault();
+    setState({ loading: true, error: null });
+    var { email, password } = document.forms[0];
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setState({ loading: false, error: null });
+        console.log(user);
+      })
+      .catch((error) => setState({ error: error.message, loading: false }));
+  };
+
+  useEffect(() => {
+    console.log(state.error);
+  }, [state.error]);
+
+  useEffect(() => {
+    if (!isLogin) signupFormRef.current.reset();
+    else loginFormRef.current.reset();
+  }, [isLogin]);
 
   return (
     <section className="relative isolate text-white overflow-hidden p-4 min-h-screen mx-auto md:px-10 max-w-7xl lg:px-8 scroll-mt-32 flex flex-col justify-center items-center">
@@ -47,7 +76,13 @@ const Auth = () => {
           </p>
         </div>
         <div className="flex-1">
-          <div className="w-fit border-2 flex flex-col rounded-2xl p-8 mx-auto">
+          <div className=" relative w-fit border-2 flex flex-col rounded-2xl p-8 mx-auto">
+            {state.loading && (
+              <div className="bg-[#000000DD] top-0 bottom-0 left-0 right-0 z-20 rounded-2xl absolute flex flex-col justify-center items-center gap-3">
+                <Image src="/assets/svgs/logo.svg" height={100} width={100} className="h-10 w-10 fill-white animate-bounce" alt="Blogopedia Logo" />
+                Please Wait
+              </div>
+            )}
             <div className="mb-6">
               <h1 className="mb-2 sm:mb-4 text-xl sm:text-2xl tracking-tight">{isLogin ? "Sign in to your account" : "Get started for free"}</h1>
               {isLogin ? (
@@ -67,14 +102,14 @@ const Auth = () => {
               )}
             </div>
             {isLogin ? (
-              <form className="sm:min-w-[270px]">
+              <form className="sm:min-w-[270px]" onSubmit={handleEmailSignIn} ref={loginFormRef} id="signin">
                 <div className="mb-4">
-                  <label htmlFor="username" />
+                  <label htmlFor="email" />
                   <input
                     className="block w-full p-4 pl-6 pr-6 bg-[#222] rounded-xl text-[#ddd] placeholder:text-[#999] truncate"
-                    placeholder="Email or Username"
-                    id="username"
-                    type="text"
+                    placeholder="Email"
+                    id="email"
+                    type="email"
                     required
                     autoComplete="email"
                   />
@@ -126,7 +161,7 @@ const Auth = () => {
                 </div>
               </form>
             ) : (
-              <form className="sm:min-w-[300px]">
+              <form className="sm:min-w-[300px]" onSubmit={handleEmailSignUp} ref={signupFormRef} id="signup">
                 <div className="mb-4">
                   <label htmlFor="username" />
                   <input
@@ -135,7 +170,7 @@ const Auth = () => {
                     id="username"
                     type="text"
                     required
-                    autoComplete="username"
+                    autoComplete="username text"
                   />
                 </div>
                 <div className="mb-4">
@@ -146,7 +181,7 @@ const Auth = () => {
                     id="email"
                     type="text"
                     required
-                    autoComplete="username"
+                    autoComplete="email"
                   />
                 </div>
                 <div className="relative mb-4">
@@ -180,17 +215,9 @@ const Auth = () => {
               <div className="text-center px-5">or</div>
               <div className="h-[0.125rem] w-full bg-[#999]"></div>
             </div>
-            <button
-              type="button"
-              className="w-full block bg-black border-2 hover:bg-white focus:text-black focus:bg-white text-white hover:text-black font-semibold rounded-lg px-4 py-3 border-white transition duration-300"
-              onClick={handleGoogleSignIn}
-            >
-              <div className="flex items-center justify-center">
-                {state.loading ? <BiLoader className="animate-spin h-5 w-5" /> : <GrGoogle className="h-4 w-4" />}
-                <span className="ml-4 ">Log in with Google</span>
-              </div>
-            </button>
+            <GoogleAuthButton />
           </div>
+          {state.error && <span>{state.error}</span>}
         </div>
       </div>
     </section>
