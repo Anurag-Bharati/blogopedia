b0c1fec8437b077049ea7b197473aa87d534a454
Anurag-Bharati <anuragbharati26@gmail.com>
2023-07-01 10:21:04 +0545

    Added confirm-action and share card for the homepage. Other minor optimization

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,41 @@
+"use client";
+
 import Featured from "@/components/Featured";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import { firestore } from "@/config/firebase/firebase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+  const [recentBlogs, setRecentBlogs] = useState([]);
+  const collectionRef = useMemo(() => collection(firestore, "blogs"), []);
+  const qRef = useMemo(() => query(collectionRef, where("status", "==", "published"), orderBy("createdAt", "desc")), [collectionRef]);
+  useEffect(() => {
+    const getBlogs = async () => {
+      if (blogs.length > 0) return;
+      try {
+        const snapshot = await getDocs(qRef);
+        setBlogs(snapshot?.docs?.map((doc) => ({ id: doc?.id, ...doc?.data() } ?? [])));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getBlogs();
+  }, [blogs, qRef]);
+
+  useEffect(() => {
+    setRecentBlogs(blogs.slice(0, 6));
+  }, [blogs]);
+
   return (
     <>
-      <Header />
+      <Header blogs={blogs} />
       <main>
-        <Hero />
-        <Featured />
+        <Hero recentBlogs={recentBlogs} />
+        <Featured blogs={blogs} />
       </main>
       <Footer />
     </>
---
+++ src/components/ShareCard.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  BiCheck,
+  BiCopyAlt,
+  BiLinkAlt,
+  BiLogoFacebookSquare,
+  BiLogoMessenger,
+  BiLogoTwitter,
+  BiLogoWhatsappSquare,
+  BiSolidEnvelope,
+  BiXCircle,
+} from "react-icons/bi";
+import { EmailShareButton, FacebookMessengerShareButton, FacebookShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
+
+const ShareCard = ({ setIsModalOpen, isModalOpen, id }) => {
+  const [state, setState] = useState({ copied: false });
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(linkField.current.value);
+    setState({ copied: true });
+  };
+
+  useEffect(() => {
+    let timeout;
+    if (state.copied) {
+      timeout = setTimeout(() => {
+        setState({ copied: false });
+        if (isModalOpen.share) setIsModalOpen({ hide: false, share: false });
+      }, 2000);
+    }
+    return () => clearTimeout(timeout);
+  }, [isModalOpen.share, setIsModalOpen, state.copied]);
+
+  const hideModal = () => setIsModalOpen({ hide: false, share: false });
+
+  const linkField = useRef(null);
+
+  return (
+    <div className=" min-w-[256px] mx-4 p-4 rounded-xl md:w-1/2 lg:w-1/3 bg-[#222]">
+      <div className="flex justify-between items center">
+        <div className="flex items-center justify-center">
+          <p className="text-xl font-medium">Share this blog?</p>
+        </div>
+        <div className=" hover:bg-red-500 cursor-pointer  font-sans w-6 h-6 flex items-center justify-center rounded-full" onClick={hideModal}>
+          <BiXCircle className="w-6 h-6 text-red-400 hover:text-[#222]" />
+        </div>
+      </div>
+      <div className="mt-2">
+        <p className="text-base pb-2">Share this link via</p>
+        <div className="flex items-center pb-2 justify-between">
+          <FacebookShareButton url={`http://localhost:3000/blogs?id=${id}`} onClick={hideModal}>
+            <BiLogoFacebookSquare className="w-8 h-8 fill-[#4267B2] rounded-md bg-white" />
+          </FacebookShareButton>
+          <FacebookMessengerShareButton url={`http://localhost:3000/blogs?id=${id}`} onClick={hideModal}>
+            <BiLogoMessenger className="w-8 h-8 rounded-md fill-[#00B2FF] bg-white" />
+          </FacebookMessengerShareButton>
+          <WhatsappShareButton url={`http://localhost:3000/blogs?id=${id}`} onClick={hideModal}>
+            <BiLogoWhatsappSquare className="w-8 h-8 rounded-md fill-[#25D366] bg-white" />
+          </WhatsappShareButton>
+          <TwitterShareButton url={`http://localhost:3000/blogs?id=${id}`} onClick={hideModal}>
+            <BiLogoTwitter className="w-8 h-8 rounded-md fill-[#1DA1F2] bg-white" />
+          </TwitterShareButton>
+          <EmailShareButton url={`http://localhost:3000/blogs?id=${id}`} onClick={hideModal}>
+            <BiSolidEnvelope className="w-8 p-0.5 h-8 rounded-md bg-white fill-gray-500" />
+          </EmailShareButton>
+        </div>
+        <p className="text-base">Or copy link</p>
+        <div className="rounded-md mb-2 border-2 border-[#444] flex justify-between items-center mt-2 py-2 px-2">
+          <BiLinkAlt className="w-6 h-6" />
+          <input
+            className="pl-2 pr-3 w-full outline-none bg-transparent truncate"
+            type="text"
+            placeholder="link"
+            value={`http://localhost:3000/blogs?id=${id}`}
+            ref={linkField}
+            readOnly
+          />
+
+          <button
+            className={` text-black  text-sm  p-1 rounded-md  ${state.copied ? "bg-green-400 hover:bg-green-500" : "text-white hover:bg-cyan-500"}`}
+            onClick={handleCopy}
+          >
+            {state.copied ? <BiCheck className="h-6 w-6 animate-pulse" /> : <BiCopyAlt className="h-6 w-6" />}
+          </button>
+        </div>
+        <p className={`text-end text-sm text-green-500 h-0 transition-all ${state.copied ? "opacity-100 h-3" : "opacity-0 pointer-events-none"}`}>
+          Copied to clipboard
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ShareCard;
